e7c3ed2c2522d24433a8aa111aaf1246949b916d
root <root@localhost>
2023-06-11 20:44:13 +0600

    auto publish 1686494653097

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
-class Phone:
-    def __init__(self, brand):
-        self.brand = brand
-    
-my_phone = Phone('SAMSUNG')
-grandma_phone = Phone('NOKIA 1100')
-print(my_phone.brand)
-print(grandma_phone.brand)
+class Bank:
+    def __init__(self):
+        self.balance = 1000
+    def get_balance(self):
+        return self.balance
+    def withdraw(self, amount):
+        self.balance = self.balance - amount
+        return amount
+            
+# now use the class
+my_bank = Bank()
+my_bank.withdraw(100)
+balance = my_bank.get_balance()
+print(balance)
+my_bank.withdraw(50)
+balance = my_bank.get_balance()
+print(balance)
