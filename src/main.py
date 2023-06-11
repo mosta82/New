79dc79a597b9dd43978d963e9da451a6522c16c6
root <root@localhost>
2023-06-11 17:41:36 +0600

    auto publish 1686483695919

--- src/main.py
+++ src/main.py
@@ -1,12 +1,8 @@
-class Laptop:
-    def __init__(self, brand):
-        self.brand = brand
-        self.age = 10
-
-    def add_year(self, increment):
-        self.age = self.age + increment
-
-
-my_lapo = Laptop('Lenovo')
-my_lapo.add_year(5)
-print(my_lapo.age)
+class Shopping:
+    cart = [ ]
+    def add_to_cart(self, item):
+        self.cart.append(item)
+        
+customer1 = Shopping()
+customer1.add_to_cart('T-shirt')
+print(customer1.cart)
